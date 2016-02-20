cc10fcb3f7858d1a802f84f474461ccd125ebf3b
LinkeZzz <linkegilmanova@gmail.com>
2016-02-20 22:46:26 +0300

    local trusted device list interface and partial realisation

--- BlueMoon/trusteddevicelist.cpp
+++ BlueMoon/trusteddevicelist.cpp
@@ -1,1 +1,79 @@
+#include "trusteddevicelist.h"
+#include <QDataStream>
+#include <QFile>
+#include <QIODevice>
+#include <QBluetoothServiceInfo>
 
+trusteddevicelist::trusteddevicelist()
+{
+
+}
+
+//Read Write
+void trusteddevicelist::readTrustedDeviceList()
+{
+    QString filename=trusteddevicelist::getFileName();
+    QString filedir= trusteddevicelist::getFileDirectory();
+
+    filename = filedir+filename;
+    QFile file(filename);
+    if (!file.open(QIODevice::ReadOnly))
+            return;
+    QVector<QString> tdlist=trusteddevicelist::getTrustedDevices();
+    QDataStream in(&file);
+    QString deviceInfo;
+    while(!in.atEnd())
+    {
+        in>>deviceInfo;
+        tdlist.prepend(deviceInfo);
+    }
+    file.close();
+    trusteddevicelist::setTrusteddevices(tdlist);
+}
+void trusteddevicelist::writeToTrustedDeviceList()
+{
+    QString filename=trusteddevicelist::getFileName();
+    QString filedir= trusteddevicelist::getFileDirectory();
+
+    filename = filedir+filename;
+    QFile file(filename);
+    if (!file.open(QIODevice::WriteOnly))
+            return;
+    QDataStream out(&file);
+    out<<QString("HTC DESIRE 616");
+    out<<QString("HP PAVILION g1349");
+    file.close();
+}
+
+// Getter Setter
+QString trusteddevicelist::getFileDirectory(){
+    return fileDirectory;
+}
+void trusteddevicelist::setFileDirectory(QString filedir){
+    fileDirectory=filedir;
+}
+
+QString trusteddevicelist::getFileName(){
+    return fileName;
+}
+void trusteddevicelist::setFileName(QString filename){
+    fileName=filename;
+}
+
+QVector<QString> trusteddevicelist::getTrustedDevices()
+{
+    return trustedDevices;
+}
+void trusteddevicelist::setTrusteddevices(QVector<QString> tD)
+{
+    trustedDevices=tD;
+}
+/*void trusteddevicelist::addToTrustList(const QBluetoothServiceInfo& serviceInfo)
+{
+    //QString device = serviceInfo.device().name().toString();
+    //device+=serviceInfo.serviceName().toString();
+    //device+=serviceInfo.address().toString();
+    //trustedDevices.prepend(deviceInfo);
+}
+void trusteddevicelist::deleteFromTrustList(const QBluetoothServiceInfo& serviceInfo)
+{}*/
--- BlueMoon/trusteddevicelist.h
+++ BlueMoon/trusteddevicelist.h
@@ -1,1 +1,36 @@
+#ifndef TRUSTEDDEVICELIST_H
+#define TRUSTEDDEVICELIST_H
+#include <QString>
+#include <QVector>
+#include <QBluetoothServiceInfo>
 
+class trusteddevicelist
+{
+public:
+    trusteddevicelist();
+
+private:
+    QString fileName;
+    QString fileDirectory;
+    QVector<QString> trustedDevices;
+
+public:
+    void setFileName(QString filename);
+    QString getFileName();
+
+    void setFileDirectory(QString filedirectory);
+    QString getFileDirectory();
+
+    void setTrusteddevices(QVector<QString> tD);
+    QVector<QString> getTrustedDevices();
+
+    void addToTrustList(const QBluetoothServiceInfo& serviceInfo);
+    void deleteFromTrustList(const QBluetoothServiceInfo& serviceInfo);
+
+public:
+    void readTrustedDeviceList();
+    void writeToTrustedDeviceList();
+
+};
+
+#endif // TRUSTEDDEVICELIST_H
